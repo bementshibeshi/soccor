fb13f9ec4abfde19071d2cf6c80b8c324d195e15
Yeajee Lee <yeajeelee@Yeajees-MacBook-Air.local>
2024-12-07 13:31:47 -0500

    first

--- team.py
+++ team.py
@@ -2,3 +2,27 @@ import unittest
 import sqlite3
 import json
 import os
+import requests
+
+def get_team_data():
+
+    API_KEY = "0cd173cf1b864ce092037aec02a7fdcb"
+    team_url = "http://api.football-data.org/v4/teams"
+    resp = requests.get(team_url) 
+    print(resp)
+
+    team_list = []
+
+    #check the if the status is valid 200 means it is okay 
+    if resp.status_code == 200:
+        #get the data content of the movie
+        data = resp.json()
+        print(data)
+        if data.get("Response") == "False":
+            return None 
+        return (data,resp.url)
+
+    #if the url is invalid
+    return None
+
+get_team_data()
