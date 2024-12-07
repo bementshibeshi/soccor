b61713b8dbf765fe376a141b21ab75867ea998fd
Yeajee Lee <yeajeelee@Yeajees-MacBook-Air.local>
2024-12-07 14:45:00 -0500

    update

--- team.py
+++ team.py
@@ -23,8 +23,25 @@ def get_comp_id():
         print(f"Error: {resp.status_code} - {resp.reason}")
         return None
 
-competition_ids = get_comp_id()
-if competition_ids:
-    print("Competition IDs:", competition_ids)
-else:
-    print("Failed to fetch competition data.")
+
+def get_comp_teams(comp_ids):
+
+    API_KEY = "0cd173cf1b864ce092037aec02a7fdcb"
+    # print(comp_ids)
+
+    for id in comp_ids[:3]:
+        # print(id)
+        team_url = f"http://api.football-data.org/v4/competitions/{id}/teams"
+        headers = {"X-Auth-Token": API_KEY}  
+        resp = requests.get(team_url, headers=headers)
+
+        if resp.status_code == 200:
+            data = resp.json()
+
+        print(data[0])
+
+
+get_comp_id()
+get_comp_teams(get_comp_id())
+
+
