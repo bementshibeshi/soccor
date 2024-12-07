a2dbc1c006d88f4925c0c64608d36c1056d4a81f
Yeajee Lee <yeajeelee@Yeajees-MacBook-Air.local>
2024-12-07 14:03:12 -0500

    parsing

--- team.py
+++ team.py
@@ -4,27 +4,31 @@ import json
 import os
 import requests
 
-def get_team_data():
+def get_comp_id():
 
     API_KEY = "0cd173cf1b864ce092037aec02a7fdcb"
-    team_url = "http://api.football-data.org/v4/teams/"
+    team_url = "http://api.football-data.org/v4/competitions/"
     headers = {"X-Auth-Token": API_KEY}
     resp = requests.get(team_url, headers=headers) 
-    print(resp)
+    # print(resp)
 
-    team_list = []
+    comp_ids = []
 
     #check the if the status is valid 200 means it is okay 
     if resp.status_code == 200:
         #get the data content of the movie
         data = resp.json()
-        print(data)
+        # print(data)
         if data.get("Response") == "False":
             return None 
-        return (data,resp.url)
+        
+    for head in data.items():
+        # print(head)
+        for info in head:
+            print(info)
+            for list in info.items():
+                print(list)
 
-    #if the url is invalid
-    return None
 
-get_team_data()
+get_comp_id()
 
