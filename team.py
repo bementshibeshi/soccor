59f9721931eeed075fe42a4cdaff1321a14d76c9
Bemnet Shibeshi <bemnet@umich.edu>
2024-12-07 15:14:22 -0500

    db

--- team.py
+++ team.py
@@ -53,3 +53,21 @@ nested_dict = get_comp_teams(comp_ids)
 
 pprint.pprint(nested_dict)
 
+def set_up_database(db_name):
+    """
+    Sets up a SQLite database connection and cursor.
+
+    Parameters
+    -----------------------
+    db_name: str
+        The name of the SQLite database.
+
+    Returns
+    -----------------------
+    Tuple (Cursor, Connection):
+        A tuple containing the database cursor and connection objects.
+    """
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + "/" + db_name)
+    cur = conn.cursor()
+    return cur, conn
