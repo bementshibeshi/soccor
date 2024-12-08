672408f5c0bab517f7810eae63ca51aee41a040c
Yeajee Lee <yeajeelee@Yeajees-MacBook-Air.local>
2024-12-08 15:41:53 -0500

    country

--- countries.py
+++ countries.py
@@ -8,20 +8,37 @@ import pprint
 
 def get_country_ids():
     API_KEY = "376c8e51eb476474a720377fd3c9c83c"
-    team_url = "https://api.soccersapi.com/v2.2/teams/?user=null&token=null&t=list&country_id=4"
-    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    page1_url = f"https://api.soccersapi.com/v2.2/countries/?user=yeajee&token={API_KEY}&t=list&page=1"
+    page2_url = f"https://api.soccersapi.com/v2.2/countries/?user=yeajee&token={API_KEY}&t=list&page=2"
+    # headers = {"Authorization": f"Bearer {API_KEY}"}
     
-    resp = requests.get(team_url, headers=headers)
-    
-    if resp.status_code == 200:
-        data = resp.json()
+    resp1 = requests.get(page1_url)
+    resp2 = requests.get(page2_url)
 
-        # Extract country IDs and names
-        countries = data.get("data", [])
+    # print(resp1, resp2)
+
+    if resp1.status_code == 200 and resp2.status_code == 200:
+        data1 = resp1.json()
+        data2 = resp2.json()
+
+        countries = data1.get("data", [])
+        countries2 = data2.get("data", [])
         country_data = [{"id": country["id"], "name": country["name"]} for country in countries]
+        country_data2 = [{"id": country["id"], "name": country["name"]} for country in countries2]
+
+        # print("This is page 1:", 
+              
+        #       country_data)
+        # print("This is page 2:"
+              
+        #       , country_data2)
+
+        country_data.extend(country_data2)
+        print(country_data)
+
         return country_data
     else:
-        print(f"Error: {resp.status_code} - {resp.reason}")
+        print(f"Error: {resp1.status_code} - {resp1.reason}")
         return None
 
 
