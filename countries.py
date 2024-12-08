43729b7f2fc2a9322e92762ac432f36fc747e5d1
Yeajee Lee <yeajeelee@Yeajees-MacBook-Air.local>
2024-12-08 17:42:35 -0500

    countries

--- countries.py
+++ countries.py
@@ -6,40 +6,38 @@ import requests
 import pprint
 
 
-def get_country_ids():
-    API_KEY = "376c8e51eb476474a720377fd3c9c83c"
-    page1_url = f"https://api.soccersapi.com/v2.2/countries/?user=yeajee&token={API_KEY}&t=list&page=1"
-    page2_url = f"https://api.soccersapi.com/v2.2/countries/?user=yeajee&token={API_KEY}&t=list&page=2"
-    # headers = {"Authorization": f"Bearer {API_KEY}"}
-    
-    resp1 = requests.get(page1_url)
-    resp2 = requests.get(page2_url)
+def get_country_names():
+    API_KEY = "32fb007eb213dce09cf03a5cb3edfc00"
+    url = "https://v3.football.api-sports.io/teams/countries"
 
-    # print(resp1, resp2)
+    headers = {
+    'x-rapidapi-key': API_KEY,
+    'x-rapidapi-host': 'v3.football.api-sports.io'
+    }
 
-    if resp1.status_code == 200 and resp2.status_code == 200:
-        data1 = resp1.json()
-        data2 = resp2.json()
+    response = requests.request("GET", url, headers=headers)
 
-        countries = data1.get("data", [])
-        countries2 = data2.get("data", [])
-        country_data = [{"id": country["id"], "name": country["name"]} for country in countries]
-        country_data2 = [{"id": country["id"], "name": country["name"]} for country in countries2]
+    #print(response)
 
-        # print("This is page 1:", 
-              
-        #       country_data)
-        # print("This is page 2:"
-              
-        #       , country_data2)
+    if response.status_code == 200:
+        data = response.json()
 
-        country_data.extend(country_data2)
-        print(country_data)
 
-        return country_data
+        country_name_list = []
+
+        for item in data.items():
+
+            response_info = data.get("response", [])
+
+            for item in response_info:
+                country_name_list.append(item["name"])
+
     else:
-        print(f"Error: {resp1.status_code} - {resp1.reason}")
+        print(f"Error: {response.status_code} - {response.reason}")
         return None
+
+    print(country_name_list)
+
 
 
 def set_up_database(db_name):
@@ -50,26 +48,24 @@ def set_up_database(db_name):
 
 
 def set_up_countries_table(data, cur, conn):
-    # Drop and create Countries table
+
     cur.execute("DROP TABLE IF EXISTS Countries")
     cur.execute("CREATE TABLE IF NOT EXISTS Countries (id INTEGER PRIMARY KEY, name TEXT UNIQUE)")
 
-    # Insert country data into the table
     for country in data:
         cur.execute(
-            "INSERT OR IGNORE INTO Countries (id, name) VALUES (?, ?)", (country["id"], country["name"])
+            "INSERT OR IGNORE INTO Countries (name) VALUES (?)", (country["name"],)
         )
     conn.commit()
 
 
 def main():
-    # Fetch country IDs and names
-    country_data = get_country_ids()
+
+    country_data = get_country_names()
     if not country_data:
         print("No country data retrieved. Exiting.")
         return
 
-    # Setup database and insert data
     cur, conn = set_up_database("206_final.db")
     set_up_countries_table(country_data, cur, conn)
     conn.close()
