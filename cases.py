8bc3b2acf4c1350640d2e7b598eaaf15471d7798
Yeajee Lee <yeajeelee@Yeajees-MacBook-Air.local>
2024-12-09 12:59:01 -0500

    update

--- cases.py
+++ cases.py
@@ -1,23 +1,69 @@
 import requests
 import json
 import pandas as pd
+import os
+import sqlite3
 
-# POST to API
-payload = {'code': 'ALL'}
-URL = 'https://api.statworx.com/covid'
-response = requests.post(url=URL, data=json.dumps(payload))
+# payload = {'code': 'ALL'}
+# URL = 'https://api.statworx.com/covid'
+# response = requests.post(url=URL, data=json.dumps(payload))
 
-# Check if the request was successful
-if response.status_code == 200:
-    data = response.json()
+# if response.status_code == 200:
+#     data = response.json()
+#     # print(data)
 
-    # Extract the relevant information: country and total cases
-    country = data['country']
-    total_cases = data['cases']
+#     country = data['country']
+#     total_cases = data['cases']
 
-    # Create a DataFrame with only country and total cases
-    df = pd.DataFrame({'Country': [country], 'Total Cases': [total_cases]})
+#     df = pd.DataFrame({'Country': [country], 'Total Cases': [total_cases]})
 
-    print(df)
-else:
-    print(f"Failed to fetch data. Status code: {response.status_code}, Reason: {response.reason}")
+#     print(df)
+# else:
+#     print(f"Failed to fetch data. Status code: {response.status_code}, Reason: {response.reason}")
+
+def set_up_database(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path + "/" + db_name)
+    conn.execute("PRAGMA foreign_keys = ON;")
+    cur = conn.cursor()
+    return cur, conn
+
+def get_code(cur, conn):
+
+    cur.execute("""SELECT country from Countries""")
+    countries = cur.fetchall()
+    print(countries)
+
+    payload = {'code': 'ALL'}
+    URL = 'https://api.statworx.com/covid'
+    response = requests.post(url=URL, data=json.dumps(payload))
+    
+    if response.status_code == 200:
+        data = response.json()
+        countrylist = []
+
+        for country in data:
+            country_name = country.get("name")
+            country_code = country.get("code")
+            if country_name and country_code:
+                countrylist.append((country_name, country_code))
+        
+        print(countrylist)
+
+        cur.execute('''ALTER TABLE Countries ADD COLUMN country_code TEXT''')
+
+        for country_name, country_code in countrylist:
+
+            cur.execute(
+                '''UPDATE Countries SET country_code = ? WHERE country = ?''', (country_code, country_name)
+            )
+
+        conn.commit()
+
+def main():
+    cur, conn = set_up_database("206_final.db")
+    get_code(cur, conn)
+    conn.close()
+
+if __name__ == "__main__":
+    main()
