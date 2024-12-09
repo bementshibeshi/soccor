a09808ecc858e2da49cd7fe7dbc7cab3c06e4a58
Yeajee Lee <yeajeelee@Yeajees-MacBook-Air.local>
2024-12-09 11:33:31 -0500

    country table

--- team.py
+++ team.py
@@ -3,6 +3,7 @@ import sqlite3
 import json
 import os
 import requests
+import pprint
 
 
 def get_comp_id():
@@ -28,7 +29,7 @@ def get_comp_id():
 
 def get_comp_teams(comp_ids):
     API_KEY = "0cd173cf1b864ce092037aec02a7fdcb"
-    comp_teams = {}  
+    countryteams = {}
 
     for comp_id in comp_ids:
         team_url = f"http://api.football-data.org/v4/competitions/{comp_id}/teams"
@@ -36,17 +37,27 @@ def get_comp_teams(comp_ids):
         resp = requests.get(team_url, headers=headers)
 
         if resp.status_code == 200:
-            data = resp.json()  
-            teams = data.get("teams", [])  
-            team_names = [team.get("shortName") for team in teams if team.get("shortName")]
-            # print(team_names)
+            data = resp.json()
+            teams = data.get("teams", [])
 
-            comp_teams[comp_id] = team_names 
+            for team in teams:
+                if team.get("shortName"):
+                    team_name = team.get("shortName")
+                    area = team.get("area", {})
+                    country_name = area.get("name")
+                    # print(country_name)
+
+                    if country_name:
+                        if country_name in countryteams:
+                            countryteams[country_name].append(team_name)
+                        else:
+                            countryteams[country_name] = [team_name]
         else:
             print(f"Failed to fetch teams for competition {comp_id}. Status code: {resp.status_code}")
-            continue
-    
-    return comp_teams
+
+    # pprint.pprint(countryteams)
+    return countryteams
+
 
 def set_up_database(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -54,11 +65,23 @@ def set_up_database(db_name):
     cur = conn.cursor()
     return cur, conn
 
+
+def set_up_countryid_table(countryteams, cur, conn):
+    cur.execute("DROP TABLE IF EXISTS Countries")
+
+    cur.execute("CREATE TABLE IF NOT EXISTS Countries (id INTEGER PRIMARY KEY, country)")
+
+    for country in countryteams.keys():
+        print(country)
+        cur.execute("INSERT OR IGNORE INTO Countries (country) VALUES (?)", (country,))
+
+    conn.commit()    
+
 def set_up_teams_table(data, cur, conn):
     cur.execute("DROP TABLE IF EXISTS Teams")
 
     cur.execute(
-        "CREATE TABLE IF NOT EXISTS Teams (id INTEGER PRIMARY KEY, name TEXT UNIQUE)"
+        "CREATE TABLE IF NOT EXISTS Teams (id INTEGER PRIMARY KEY, name TEXT UNIQUE, country_id INTEGER)"
     )
     
     # print(data)
@@ -72,10 +95,10 @@ def set_up_teams_table(data, cur, conn):
                 cur.execute(
                     "INSERT OR IGNORE INTO Teams (name) VALUES (?)", (team_name,)
                 )
-
             
-    
     conn.commit()
+
+
 
 
 def main():
@@ -85,6 +108,7 @@ def main():
 
     cur, conn = set_up_database("206_final.db")
     set_up_teams_table(data, cur, conn)
+    set_up_countryid_table(data, cur, conn)
     conn.close()
 
 if __name__ == "__main__":
