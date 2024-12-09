a344cb3311ec7ef7c9289424921a3c84e18c8b31
Yeajee Lee <yeajeelee@Yeajees-MacBook-Air.local>
2024-12-08 21:57:05 -0500

    I didn't add much and there are failures

--- countries.py
+++ countries.py
@@ -5,9 +5,10 @@ import os
 import requests
 import pprint
 
+API_KEY = "32fb007eb213dce09cf03a5cb3edfc00"
+
 
 def get_country_names():
-    API_KEY = "32fb007eb213dce09cf03a5cb3edfc00"
     url = "https://v3.football.api-sports.io/teams/countries"
 
     headers = {
@@ -25,20 +26,48 @@ def get_country_names():
 
         country_name_list = []
 
-        for item in data.items():
 
-            response_info = data.get("response", [])
+        response_info = data.get("response", [])
 
-            for item in response_info:
-                country_name_list.append(item["name"])
+        for item in response_info:
+            country_name_list.append(item["name"])
 
     else:
         print(f"Error: {response.status_code} - {response.reason}")
         return None
 
-    print(country_name_list)
+    # print(country_name_list)
+    return country_name_list
 
+def teams_to_countries(country_names):
+    
+    team = {}
 
+    for country in country_names:
+        url = f"https://v3.football.api-sports.io/teams/country={country}"
+        headers = {
+        'x-rapidapi-key': API_KEY,
+        'x-rapidapi-host': 'v3.football.api-sports.io'
+        }
+
+        response = requests.request("GET", url, headers=headers)
+
+        if response.status_code == 200:
+            data = response.json()
+                
+            response_info = data.get("response", [])
+
+            for item in response_info:
+                
+                if item["country"] in team:
+
+                    team["country"] = item["name"]
+
+                else:
+                    team["country"] = item["name"]
+
+    print(team)
+    return team
 
 def set_up_database(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -52,9 +81,10 @@ def set_up_countries_table(data, cur, conn):
     cur.execute("DROP TABLE IF EXISTS Countries")
     cur.execute("CREATE TABLE IF NOT EXISTS Countries (id INTEGER PRIMARY KEY, name TEXT UNIQUE)")
 
-    for country in data:
+    for country in data.items():
+        countryname = country["name"]
         cur.execute(
-            "INSERT OR IGNORE INTO Countries (name) VALUES (?)", (country["name"],)
+            "INSERT OR IGNORE INTO Countries (name) VALUES (?)", (countryname,)
         )
     conn.commit()
 
