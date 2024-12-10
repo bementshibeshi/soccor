5dd541217c32e662c888aee0e45af4ceed872b69
Yeajee Lee <yeajeelee@Yeajees-MacBook-Air.local>
2024-12-10 11:20:45 -0500

    new

--- games.py
+++ games.py
@@ -1,9 +1,9 @@
-# import unittest
-# import sqlite3
-# import json
-# import os
-# import requests
-# import pprint
+import unittest
+import sqlite3
+import json
+import os
+import requests
+import pprint
 
 # def get_games_cancelled():
 
@@ -19,3 +19,19 @@
 #     response = requests.get(url, headers=headers)
 
 #     print(response.json())
+
+import requests
+
+url = "https://free-api-live-football-data.p.rapidapi.com/football-get-matches-by-date"
+
+querystring = {"date":"20200625"}
+
+headers = {
+	"x-rapidapi-key": "acf0777e33msha5e9de947da5ee5p1797f8jsnbb4a8ec0bba6",
+	"x-rapidapi-host": "free-api-live-football-data.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.json())
+
